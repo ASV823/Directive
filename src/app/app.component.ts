be97a8e9d01e7f27bfaf1d29a8895ad89fad3f99
Sai Vikas Addanki <saivikasaddanki@Sais-MacBook-Air.local>
2024-09-25 19:36:13 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'profile';
+  isLoggedIn : boolean = true;
+  userName : string = "Vikas";
+
+  isAdmin : boolean= false;
+  isMember : boolean = false;
+  isGuest : boolean = true
+
+  loginCount : number = 0;
+  countLoginAttempts() {
+    this.loginCount ++;
+    console.log(this.loginCount);
+  }
+
+  userRole : string ="Member";
 }
