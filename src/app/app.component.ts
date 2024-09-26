25fa3b19834fcd1d278b376d535b88715a34ae11
Sai Vikas Addanki <saivikasaddanki@Sais-MacBook-Air.local>
2024-09-26 18:22:53 +0530

    updates of 26

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf],
+  imports: [RouterOutlet, NgIf,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -24,5 +24,32 @@ export class AppComponent {
     console.log(this.loginCount);
   }
 
-  userRole : string ="Member";
+  userRole : string ="Admin";
+
+  users : Array<string> = ['John','Vikas','Vishnu','Daksh'];
+
+  userObject : Array<any> = [
+    {id: 1, name : 'Raj' ,email:'raj@gmail.com'},
+    {id: 2, name : 'Ravi' ,email:'ravi@gmail.com'},
+    {id: 3, name : 'Kumar' ,email:'kumar@gmail.com'},
+    {id: 4, name : 'Kiran' ,email:'kiran@gmail.com'},
+  ]
+    
+  addNewUser() {
+    let user = {id:5, name:'jon', email:'jon@gmail.com'};
+    this.userObject.push(user);
+  }
+
+  removeUser() {
+    this.userObject.pop();
+  }
+
+  // Array length index
+
+  constructor(){
+    console.log(this.userObject.length);
+    
+  }
+
+  userSwitch :string = "Admin";
 }
